64791d49efc2f5fd82e5333e8ff6a262fda4b867
Andrew Amis <atamiser@gmail.com>
2018-06-18 21:47:51 -0700

    Added basic demonstration object and camera controls.

---
+++ Assets/src/CameraController.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    Camera cam;
+    void Start() {
+        cam = Camera.main;
+        cam.orthographic = false;
+    }
+    
+    void Update() {
+        var trans = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.A)) {
+            trans = trans + new Vector3(-1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            trans = trans + new Vector3(1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W)) {
+            trans = trans + new Vector3(0, 1, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+            trans = trans + new Vector3(0, -1, 0);
+        }
+
+        trans = trans + new Vector3(0, 0, Input.GetAxis("Mouse ScrollWheel"));
+
+        // Normalize so diagonal movement isn't bizarrely fast
+        gameObject.transform.Translate(trans.normalized * 0.2f);
+    }
+}
---
+++ Assets/src/Main.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Main : MonoBehaviour {
+
+    GameObject square;
+
+	// Use this for initialization
+	void Start () {
+        var cam = Camera.main;
+
+        cam.gameObject.AddComponent<CameraController>();
+
+        square = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        square.transform.position = new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var deltaX = (float) (Math.Cos(Time.time) * 0.5);
+
+        square.transform.Translate(deltaX, 0, 0);
+	}
+}
