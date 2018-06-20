df0c26973420076d150e53ae0b6ae4e7a49e5b70
Andrew Amis <atamiser@gmail.com>
2018-06-19 20:22:31 -0700

    Refactored mesh code out of ground.

--- Assets/src/Ground.cs
+++ Assets/src/Ground.cs
@@ -6,86 +6,30 @@ using NavMeshBuilder = UnityEngine.AI.NavMeshBuilder;
 
 
 public class Ground : MonoBehaviour {
+    /*
+     * TODO: bake obstacles into level mesh, see https://catlikecoding.com/unity/tutorials/procedural-grid/.
+     **/
 
-    public Vector3[] verts;
-    public Vector2[] uv;
-    public int[] tris;
-    Mesh mesh;
+
+    NavMeshSurface nms;
 
     public static Ground Create() {
         var go = new GameObject("Ground");
         var ground = go.AddComponent<Ground>();
 
-        ground.verts = new[]{
-            new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 0, 0)
-        };
 
-        ground.verts = new[]{
-            new Vector3(0, 0, 0), new Vector3(0, 0, 20), new Vector3(20, 0, 0)
-        };
+        SimpleQuad.ImmediateAdd(go, 20, 20);
 
-        ground.uv = new[] { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1) };
-        ground.tris = new int[] { 0, 1, 2};
-
-        var mesh = ground.mesh = new Mesh();
-
-        go.AddComponent<MeshFilter>().mesh = ground.mesh;
-        var renderer = go.AddComponent<MeshRenderer>();
-
-        mesh.Clear();
-
-        mesh.vertices = ground.verts;
-        mesh.uv = ground.uv;
-        mesh.triangles = ground.tris;
-        mesh.colors = new Color[] { new Color(0, 0, 0), new Color(0, 0, 0), new Color(1, 1, 1) };
-        mesh.RecalculateBounds();
-        mesh.RecalculateNormals();
-        mesh.RecalculateTangents();
-
-        renderer.material.mainTexture = makeColorTexture();
-        renderer.material.shader = Shader.Find("Unlit/Texture");
-
+        // MeshCollider has trouble with local scale changes, even when added after the scale changes.
         go.AddComponent<MeshCollider>();
 
+        ground.nms = go.AddComponent<NavMeshSurface>();
 
-        var navSurface = go.AddComponent<NavMeshSurface>();
-
-        navSurface.BuildNavMesh();
-
-        //go.transform.Rotate(new Vector3(-90, 0, 0));
-
-        /*ground.navMeshData = new NavMeshData();
-        ground.instance = NavMesh.AddNavMeshData(ground.navMeshData);
-        var defaultBuildSettings = NavMesh.GetSettingsByID(0);
-
-
-        List<NavMeshBuildSource> sources = new List<NavMeshBuildSource>();
-
-        var s = new NavMeshBuildSource();
-        s.shape = NavMeshBuildSourceShape.Mesh;
-        s.sourceObject = mesh;
-        s.transform = go.transform.localToWorldMatrix;
-        s.area = 0;
-
-        sources.Add(s);
-
-        var bounds = new Bounds(go.transform.position, new Vector3(1, 1, 1));
-
-        NavMeshBuilder.UpdateNavMeshData(ground.navMeshData, defaultBuildSettings, sources, bounds);
-
-    */
+        ground.nms.BuildNavMesh();
+        
         return ground;
     }
-
-    private static Texture2D makeColorTexture() {
-        var tex = new Texture2D(1, 1);
-        tex.SetPixel(0, 0, Color.white);
-
-        return tex;
-    }
-
-    NavMeshData navMeshData;
-    NavMeshDataInstance instance;
+    
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/src/SimpleQuad.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleQuad {
+
+    public static MeshFilter ImmediateAdd(GameObject go, float height, float width) {
+
+        var filter = go.GetComponent<MeshFilter>();
+        if (!filter) {
+            filter = go.AddComponent<MeshFilter>();
+        }
+        var renderer = go.GetComponent<MeshRenderer>();
+        if (!renderer) {
+            renderer = go.AddComponent<MeshRenderer>();
+        }
+
+        var verts = new[]{
+            new Vector3(0, 0, 0), new Vector3(0, 0, height), new Vector3(width, 0, 0), new Vector3(width, 0, height)
+        };
+        
+        for (int i = 0; i < verts.Length; i++) {
+            verts[i] = verts[i] + new Vector3(-width / 2, 0, -height / 2);
+        }
+
+        var uv = new[] { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 0), new Vector2(1, 1) };
+        var tris = new int[] { 0, 1, 2, 3, 2, 1 };
+
+        var mesh = new Mesh();
+
+        filter.mesh = mesh;
+
+        mesh.Clear();
+
+        mesh.vertices = verts;
+        mesh.uv = uv;
+        mesh.triangles = tris;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+
+        renderer.material.mainTexture = makeColorTexture();
+        renderer.material.shader = Shader.Find("Unlit/Texture");
+
+        return filter;
+    }
+
+
+    private static Texture2D makeColorTexture() {
+        var tex = new Texture2D(1, 1);
+        tex.SetPixel(0, 0, Color.white);
+
+        return tex;
+    }
+}
