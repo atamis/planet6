a046f912d825dfe20de3724c39c287d2a1344f5a
Andrew Amis <atamiser@gmail.com>
2018-07-08 17:33:55 -0700

    "Fixed" a bug related to mesh reassignment.
    As per the comment, it isn't fixed, but the error is under control, and doesn't seem to be damaging. All the physics keeps chugging afterwards.

--- Assets/src/lighting/LightHalo.cs
+++ Assets/src/lighting/LightHalo.cs
@@ -19,6 +19,7 @@ public class LightHalo : MonoBehaviour {
         lightModel.transform.localPosition = Layers.ModelCharacter; // TODO: maybe not the character layer?
         lightModel.transform.localRotation = Quaternion.Euler(0, 0, 0);
         lightModel.layer = 8;
+        lightModel.AddComponent<MeshCollider>();
 
         lastRadius = radius - 1; // Force quad remake in Update()
     }
@@ -32,8 +33,15 @@ public class LightHalo : MonoBehaviour {
             SimpleQuad.ImmediateAdd(lightModel, radius, radius);
             lastRadius = radius;
             var mr = lightModel.GetComponent<MeshRenderer>();
-            lightModel.AddComponent<MeshCollider>();
             mr.material = new Material(Shader.Find("Custom/Circle2"));
+
+            var mf = lightModel.GetComponent<MeshFilter>();
+            var mc = lightModel.GetComponent<MeshCollider>();
+            mc.sharedMesh = mf.sharedMesh;
+            // TODO: this causes a "cleaning the mesh failed" error
+            // sometimes. This is related to 0 size meshes (like when
+            // the lighthalo radius is 0), but a fix is not immediately
+            // apparent.
         }
     }
 
