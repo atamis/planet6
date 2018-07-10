ddc248f71f9e68080cf71ded31d1937a1a2605e9
Andrew Amis <atamiser@gmail.com>
2018-07-09 20:00:46 -0700

    Added power-light combo behavior
    Powered light is such a common and obvious combination, but simply adding both behaviors to an object won't produce the desired effect, it needs extra code run every frame. LightBuilding did this properly, but with extra building-related code, so I refactored the light relevant code into its own behavior. As it turns out, this code needed substantial generalization, and revealed that turning lights off by setting their radii to 0 is a bad and brittle idea.

--- Assets/src/buildings/LightBuilding.cs
+++ Assets/src/buildings/LightBuilding.cs
@@ -3,28 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Building))]
-[RequireComponent(typeof(LightHalo))]
+[RequireComponent(typeof(PowerableLightHalo))]
 public class LightBuilding : MonoBehaviour {
 
-    Powerable powerable;
-    LightHalo lightHalo;
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         var wo = GetComponent<WorldObject>();
         // TODO: don't use capacitor sprite for simple light.
         wo.sprite = Main.atlas.GetSprite("capacitor");
 
-        powerable = GetComponent<Powerable>();
-        lightHalo = GetComponent<LightHalo>();
+        var halo = GetComponent<LightHalo>();
+        halo.radius = 5;
     }
 
     // Update is called once per frame
     void Update () {
-		if (powerable.powered) {
-            lightHalo.radius = 5;
-        } else {
-            lightHalo.radius = 0;
-        }
 	}
 }
---
+++ Assets/src/buildings/power/PowerableLightHalo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LightHalo))]
+[RequireComponent(typeof(Powerable))]
+public class PowerableLightHalo : MonoBehaviour {
+
+    Powerable powerable;
+    LightHalo lightHalo;
+
+    private float oldRadius;
+
+    // Use this for initialization
+    void Start () {
+        powerable = GetComponent<Powerable>();
+        lightHalo = GetComponent<LightHalo>();
+        oldRadius = lightHalo.radius;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // TODO: maybe make disabling a light easier?
+        // TODO: How will this interact with flickering?
+        if (lightHalo.radius != 0) {
+            oldRadius = lightHalo.radius;
+        }
+
+        if (powerable.powered) {
+            lightHalo.radius = oldRadius;
+        } else {
+            lightHalo.radius = 0;
+        }
+    }
+}
